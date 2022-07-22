f393d001a41752a70414e100056e32dc30e30f43
dimitarmitkov <dimitar.mitkov@me.com>
2022-07-22 12:19:37 +0300

    add thread code clean up

--- client/src/components/threads/CreateThread.jsx
+++ client/src/components/threads/CreateThread.jsx
@@ -1,37 +1,16 @@
 import {Container, Row, Col} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
-import {Button} from 'primereact/button';
 import "./singleThreadCardStyle.css";
 import {valuesLinks} from "../../enumerators/links";
-import {useEffect, useState} from "react";
-import axios from "axios";
 import avatar1 from "../../images/avatar2.png";
-import {AutoComplete} from 'primereact/autocomplete';
 
 
 const SingleThreadForList = () => {
     const navigate = useNavigate();
 
-    const [threadData, setThreadData] = useState([]);
-
-    const getData = async () => {
-
-        axios.get("http://localhost:62000/threads")
-            .then(response => {
-                setThreadData(response.data);
-            })
-            .catch(error => console.log(error));
-    }
-
-    useEffect(() => {
-        getData();
-    }, []);
-
     const clickHandler = () => {
-
         navigate(valuesLinks.StartTask);
     }
-
 
     return (
         <>
@@ -40,12 +19,10 @@ const SingleThreadForList = () => {
                     <Col md={2}>
                         <Row>
                             <img className={"avatar-images-start"} src={avatar1} alt="avatar"/>
-
                         </Row>
                     </Col>
                     <Col md={10}>
                         <Row id="posted" className={"card-container-thread-posted"}>
-                            {/*<AutoComplete className={"create-post"} placeholder="Create Post"  />*/}
                             <div className={"create-post"}>Create Post</div>
                         </Row>
 
